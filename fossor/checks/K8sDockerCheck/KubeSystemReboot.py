1f0c50dcc5fc27e7bfa7507045b4e6e4f6f5bacf
wanghongjie <hjwang@guandata.com>
2019-08-27 19:34:42 +0800

    修改插件

--- fossor/checks/K8sDockerCheck/KubeSystemReboot.py
+++ fossor/checks/K8sDockerCheck/KubeSystemReboot.py
@@ -7,7 +7,7 @@ class KubeSystemReboot(Check):
         out, err, return_code = self.shell_call('kubectl get pods --all-namespaces')
         pods = out.splitlines()[1:]
         kubeSystemPods = [pod.split()[1] for pod in pods if "kube-system" in pod and "Running" in pod]
-        pattern = re.compile(r'(.*?)State:(.*?)Running(.*?)Started:(.*?)\+0800', re.S | re.I)
+        pattern = re.compile(r'(.*?)State:(.*?)Running(.*?)Started:(.*?)Last', re.S | re.I)
         for kubeSystemPod in kubeSystemPods:
             if self.checkIfRebootInLast24Hours(kubeSystemPod, pattern):
                 return '过去24h内，kube-system组件: %s 发生过重启，可能是kubelet异常，请检查' % (kubeSystemPod)
@@ -15,7 +15,7 @@ class KubeSystemReboot(Check):
 
     def checkIfRebootInLast24Hours(self, systemPodTocheck, pattern):
         out, err, return_code = self.shell_call('kubectl describe pod %s -nkube-system' % (systemPodTocheck))
-        startedTimeStr = re.match(pattern, ''.join(out)).group(4).strip()
+        startedTimeStr = re.match(pattern, ''.join(out)).group(4).strip()[0:-5].strip()
         startedTime = datetime.datetime.strptime(startedTimeStr, "%a, %d %b %Y %H:%M:%S")
         nowDay = datetime.datetime.today()
         return (nowDay-startedTime).days < 1
